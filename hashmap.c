3f4580bf26a55472cb5b9382145b22e1e305c37d
Replit user <>
2023-09-07 18:08:24 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -45,7 +45,7 @@ void insertMap(HashMap * map, char * key, void * value) {
   newPair->key = strdup(key); 
   newPair->value = value;
 
-  map->buckets[2] = newPair;
+  map->buckets[3] = newPair;
   map->size++;
 }
 
